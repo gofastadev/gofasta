9401eae278e741972651aa51f66d7a3161ea1d06
descholar-ceo <emmanuellamugi@gmail.com>
2024-08-03 01:02:27 +0200

    Add random password

--- app/services/user.service.go
+++ app/services/user.service.go
@@ -1,8 +1,11 @@
 package services
 
 import (
+	"log"
+
 	"github.com/healtronlabs/go_gql_template/app/graphql/goTypes"
 	"github.com/healtronlabs/go_gql_template/app/models"
+	"github.com/healtronlabs/go_gql_template/app/utils"
 	"gorm.io/gorm"
 )
 
@@ -24,12 +27,17 @@ func (u *UserService) GetUsers() ([]*goTypes.User, error) {
 }
 
 func (u *UserService) CreateUser(input goTypes.NewUserDto) (*goTypes.User, error) {
+	randomPassword, err := utils.GeneratePassword(16)
+	if err != nil {
+		log.Printf("Error while generating a random password: %v", err)
+		return nil, err
+	}
 	userData := models.User{
 		FirstName: input.FirstName,
 		OtherNames: input.OtherNames,
 		PhoneNumber: input.PhoneNumber,
 		Email: input.Email,
-		Password: "Just a random password",
+		Password: randomPassword,
 	}
 	result := u.DB.Create(&userData)
 	user := &goTypes.User{
