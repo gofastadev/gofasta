baa4083f98fe7b4fe9fd99b52b6ccdd29986bfe9
descholar-ceo <emmanuellamugi@gmail.com>
2025-01-24 11:19:06 +0200

    ft: Add validators

---
+++ app/validators/validate-input.go
@@ -0,0 +1,29 @@
+package validators
+
+import (
+	"github.com/go-playground/validator/v10"
+	"github.com/healtronlabs/go_gql_template/app/dtos"
+	"github.com/healtronlabs/go_gql_template/app/utils"
+	"gorm.io/gorm"
+)
+
+// ValidateInput validates the input struct and returns any validation errors
+func ValidateInput(input interface{}, db *gorm.DB) []*dtos.TCommonAPIErrorDto {
+	validate, trans, err := newValidator(db)
+	if err != nil {
+		return []*dtos.TCommonAPIErrorDto{{Message: "Validation initialization error"}}
+	}
+
+	valErrs := []*dtos.TCommonAPIErrorDto{}
+	validationError := validate.Struct(input)
+	if validationError != nil {
+		if validationErrors, ok := validationError.(validator.ValidationErrors); ok {
+			for _, vErr := range validationErrors {
+				fieldName := utils.ConvertUpperCamelToLowerCamel(vErr.Field())
+				valErrs = append(valErrs, &dtos.TCommonAPIErrorDto{Message: vErr.Translate(trans), FieldName: &fieldName})
+			}
+		}
+	}
+
+	return valErrs
+}
