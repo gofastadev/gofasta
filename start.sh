595bdf9e8bdc7d2eb5074864045acd56bed45368
descholar-ceo <emmanuellamugi@gmail.com>
2025-08-17 10:08:45 +0200

    ft: Pin golang-migrate to stick with golang 1.22.5

--- start.sh
+++ start.sh
@@ -12,8 +12,8 @@ if [ -z "$1" ]; then
 else
   if [[ "$1" == "dev" || "$1" == "qa" || "$1" == "prod" ]]; then
     echo "Starting gofasta in: $1 environment..."
-    docker compose -p gofasta_helpers -f "./compose-$1.yml" --profile db up -d
-    docker compose -p gofasta_full_setup -f "./compose-$1.yml" --profile main up
+    docker compose -p gofasta_helpers -f "./compose-$1.yml" --env-file .env --profile db up -d
+    docker compose -p gofasta_full_setup -f "./compose-$1.yml" --env-file .env --profile main up
   else
     echo "Invalid environment: $1, valid environments are: dev, qa, prod"
   fi
